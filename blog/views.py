1a3923be6e707c614616e00c92e7b830350f8626
Suo-0 <131347382+Suo-0@users.noreply.github.com>
2026-07-29 03:48:07 +0900

    Exercise 6: writing view functions fixes #3

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+from .models import Post
+
+
+def _format_posts(posts):
+	lines = [f"{post.title} by {post.author.username}" for post in posts]
+	return HttpResponse("\n".join(lines) if lines else "No posts yet.")
+
+
+def post_list(request):
+	posts = Post.objects.select_related("author").all()
+	return _format_posts(posts)
+
+
+def post_list_by_author(request, username):
+	posts = Post.objects.select_related("author").filter(author__username=username)
+	return _format_posts(posts)
+
+
+def post_list_by_date(request, date_str):
+	date = datetime.strptime(date_str, "%Y-%m-%d").date()
+	posts = Post.objects.select_related("author").filter(created_at__date=date)
+	return _format_posts(posts)
+
+
+def post_create_dummy(request):
+	user_model = get_user_model()
+	user, _ = user_model.objects.get_or_create(username="dummy")
+	user.set_unusable_password()
+	user.save(update_fields=["password"])
+	Post.objects.create(
+		author=user,
+		title="Dummy Post",
+		content="This is a dummy post.",
+	)
+	return redirect("post-list")
